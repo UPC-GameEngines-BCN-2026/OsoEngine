1be81490c7c0ab70b022eb20c663bb2c3d5e0bc4
OscarEscF <oscarescofet823@gmail.com>
2026-09-21 23:41:50 +0200

    visual studio project

---
+++ OsoEngine/OsoEngine/OsoEngine.cpp
@@ -0,0 +1,12 @@
+﻿// OsoEngine.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "OsoEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
