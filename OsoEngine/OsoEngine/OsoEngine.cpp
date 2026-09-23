e10f45617ec80564bd1c95b735806aec276fe7ad
OscarEscF <oscarescofet823@gmail.com>
2026-09-23 17:16:12 +0200

    sdl3 /glad

--- OsoEngine/OsoEngine/OsoEngine.cpp
+++ OsoEngine/OsoEngine/OsoEngine.cpp
@@ -1,4 +1,6 @@
 ﻿#include "OsoEngine.h"
+#include <SDL3/SDL.h>
+#include <glad/glad.h>
 #include <glm/glm.hpp>
 
 
@@ -8,11 +10,100 @@ int main()
 {
     glm::vec3 position(1.0f, 2.0f, 3.0f);
 
-    std::cout << "OsoEngine iniciado!\n";
-    std::cout << "Position: "
-        << position.x << ", "
-        << position.y << ", "
-        << position.z << "\n";
+    std::cout << "OsoEngine iniciado\n";
+
+    //Iniciar SDL 
+    if (!SDL_Init(SDL_INIT_VIDEO))
+    {
+        std::cerr << "SDL_Init fail: "
+            << SDL_GetError()
+            << std::endl;
+
+        return 1;
+    }
+
+    //Configurar OpenGL
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+    SDL_GL_SetAttribute(
+        SDL_GL_CONTEXT_PROFILE_MASK,
+        SDL_GL_CONTEXT_PROFILE_CORE
+    );
+    SDL_Window* window = SDL_CreateWindow(
+        "OsoEngine - OpenGL",
+        1280,
+        720,
+        SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE
+    );
+    if (window == nullptr)
+    {
+        std::cerr << "SDL_CreateWindow fail: "
+            << SDL_GetError()
+            << std::endl;
+
+        SDL_Quit();
+        return 1;
+    }
+    SDL_GLContext glContext = SDL_GL_CreateContext(window);
+
+    if (glContext == nullptr)
+    {
+        std::cerr << "SDL_GL_CreateContext failed: "
+            << SDL_GetError()
+            << std::endl;
+
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+
+        return 1;
+    }
+    if (!gladLoadGLLoader(
+        reinterpret_cast<GLADloadproc>(SDL_GL_GetProcAddress)))
+    {
+        std::cerr << "Failed to initialize GLAD"
+            << std::endl;
+
+        SDL_GL_DestroyContext(glContext);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+
+        return 1;
+    }
+
+
+bool running = true;
+
+    while (running)
+    {
+        SDL_Event event;
+
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_EVENT_QUIT)
+            {
+                running = false;
+            }
+        }
+
+        glClearColor(
+            0.1f,
+            0.1f,
+            0.15f,
+            1.0f
+        );
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        SDL_GL_SwapWindow(window);
+
+        SDL_Delay(16);
+    }
+
+    // Cleanup
+
+    SDL_GL_DestroyContext(glContext);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }
